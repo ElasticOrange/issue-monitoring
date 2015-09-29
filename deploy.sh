9e5ba57d9dffe96eded5f521cec7165e5ecc27ee
Mihai Barbu <xwave21@gmail.com>
2015-09-29 17:16:53 +0300

    Add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+if [ -z "$TRAVIS_BRANCH" ]
+    then
+        TRAVIS_BRANCH="$(git symbolic-ref -q --short HEAD)"
+fi
+
+if [ $TRAVIS_BRANCH == 'master' ]
+    then
+        echo "Deploying live"
+        envoy run deploy --on=master --branch=master
+        # curl -X POST --data-urlencode 'payload={"channel": "#deployment-tracking", "username": "Deploy Truck", "text": "Updated http://staging.lendia.ro", "icon_emoji": ":deploytruck:"}' https://hooks.slack.com/services/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
+fi
+
+if [ $TRAVIS_BRANCH == 'staging' ]
+    then
+        echo "Deploying staging"
+        envoy run deploy --on=staging --branch=staging
+        # curl -X POST --data-urlencode 'payload={"channel": "#deployment-tracking", "username": "Deploy Truck", "text": "Updated https://lendia.ro", "icon_emoji": ":deploytruck:"}' https://hooks.slack.com/services/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
+fi
