35cd32571a088be5c5038c660b251f2bdc681b01
Mihai Barbu <xwave21@gmail.com>
2015-10-09 13:15:33 +0300

    Fixed master layout

--- resources/views/admin/layouts/partials/header.blade.php
+++ resources/views/admin/layouts/partials/header.blade.php
@@ -1,20 +1,30 @@
-<ul class="nav navbar-top-links navbar-right">
-    <!-- /.dropdown -->
+<ul class="nav navbar-top-links navbar-right" style="margin-right: 0px;">
     <li class="dropdown">
         <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-            <i class="fa fa-user fa-fw"></i>  <i class="fa fa-caret-down"></i>
+            <i class="fa fa-user fa-fw"></i>
+            <i class="fa fa-caret-down"></i>
         </a>
         <ul class="dropdown-menu dropdown-user">
-            <li><a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>
+            <li>
+                <a href="#">
+                    <i class="fa fa-user fa-fw"></i>
+                    User Profile
+                </a>
             </li>
-            <li><a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
+            <li>
+                <a href="#">
+                    <i class="fa fa-gear fa-fw"></i>
+                    Settings
+                </a>
             </li>
-            <li class="divider"></li>
-            <li><a href="/admin/login"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
+            <li class="divider">
+            </li>
+            <li>
+                <a href="/admin/login">
+                    <i class="fa fa-sign-out fa-fw"></i>
+                    Logout
+                </a>
             </li>
         </ul>
-        <!-- /.dropdown-user -->
     </li>
-    <!-- /.dropdown -->
 </ul>
-<!-- /.navbar-top-links -->
