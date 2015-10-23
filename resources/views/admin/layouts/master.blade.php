840019f097b40af7772dcb2b2ce8bb950aa47e07
Mihai Barbu <xwave21@gmail.com>
2015-10-23 12:01:39 +0300

    Coment GA script

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -38,6 +38,7 @@
     </div>
 
             <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+{{--
     <script>
     (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
                 function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
@@ -46,6 +47,7 @@
                 r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
             ga('create','UA-XXXXX-X','auto');ga('send','pageview');
     </script>
+--}}
 
 </body>
 </html>
