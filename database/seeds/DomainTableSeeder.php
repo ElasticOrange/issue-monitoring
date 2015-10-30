de0c834f4e3174d5b8b009af3aeba026d510bafc
Alex Urucu <quigonjin2@gmail.com>
2015-10-30 09:31:56 +0200

    Remove data generation from domains table seeder

--- database/seeds/DomainTableSeeder.php
+++ database/seeds/DomainTableSeeder.php
@@ -21,19 +21,5 @@ class DomainTableSeeder extends Seeder
         }
 
         $domain->save();
-
-        foreach (range(1, 3) as $index) {
-	        $domain = \Issue\Domain::create([
-	            'parent_id' => 1,
-	        ]);
-
-	        $string = str_random(6);
-	        $domainNames = [ 'ro' => $string.'_ro', 'en' => $string.'_en'];
-	        foreach (['ro', 'en'] as $locale) {
-	            $domain->translateOrNew($locale)->name = $domainNames[$locale];
-	        }
-
-	        $domain->save();
-        }
     }
 }
