fe378109f7375663d412ad49d376f3cf7abaa372
Mihai Barbu <xwave21@gmail.com>
2016-03-31 16:06:22 +0300

    Edit buttons should generally open in new tab

--- resources/views/admin/backend/issues/action_buttons.blade.php
+++ resources/views/admin/backend/issues/action_buttons.blade.php
@@ -1,6 +1,6 @@
 <script id="action_buttons" type="text/template">
 	<div style="white-space: nowrap;">
-		<a href="{{ action($controller.'@edit', ['__id__']) }}" class="btn btn-primary">
+		<a href="{{ action($controller.'@edit', ['__id__']) }}" class="btn btn-primary" target="_blank">
 			<span class="glyphicon glyphicon-pencil"></span>
 		</a>
 		<a href="{{ action($controller.'@destroy', ['__id__']) }}" class="btn btn-danger delete-button">
