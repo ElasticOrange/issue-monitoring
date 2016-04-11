02756a38bf2ac10de38ce214d5ee449df95951c8
Mihai Barbu <xwave21@gmail.com>
2016-04-11 15:32:32 +0300

    Remove carousel

--- resources/views/frontend/pages/homepage.blade.php
+++ resources/views/frontend/pages/homepage.blade.php
@@ -6,27 +6,7 @@
     @include('frontend.layout.header')
     <div class="row">
         <div class="col-md-12">
-            <div class="carousel slide" id="carousel-122866">
-                <ol class="carousel-indicators">
-                    <li class="active" data-slide-to="0" data-target="#carousel-122866">
-                    </li>
-                    <li data-slide-to="1" data-target="#carousel-122866">
-                    </li>
-                    <li data-slide-to="2" data-target="#carousel-122866">
-                    </li>
-                </ol>
-                <div class="carousel-inner">
-                    <div class="item active">
-                        <img alt="Slide IM" src="/img/slider1.png" />
-                    </div>
-                    <div class="item">
-                        <img alt="Carousel Bootstrap Second" src="/img/slider1.png" />
-                    </div>
-                    <div class="item">
-                        <img alt="Carousel Bootstrap Third" src="/img/slider1.png" />
-                    </div>
-                </div> <a class="left carousel-control" href="#carousel-122866" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a> <a class="right carousel-control" href="#carousel-122866" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
-            </div>
+            <img alt="Slide IM" src="/img/slider1.png" />
         </div>
     </div>
 
