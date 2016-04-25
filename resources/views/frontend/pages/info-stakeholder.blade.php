df8f98f3edf7e49969a0dad67602e4713306e0aa
Mihai Barbu <xwave21@gmail.com>
2016-04-25 11:06:20 +0300

    Stakeholderi (textul pe o suprafață mai întinsă, mai puțin centrat decât acum)

--- resources/views/frontend/pages/info-stakeholder.blade.php
+++ resources/views/frontend/pages/info-stakeholder.blade.php
@@ -5,7 +5,7 @@
     <div class="container white">
         @include('frontend.layout.header')
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
+            <div class="col-md-10 col-md-offset-1">
                 @include('frontend.partials.stakeholder-details')
             </div>
 
