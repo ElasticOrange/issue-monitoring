a12396d525da405e31f0795bb8b3842b64e5800d
Mihai Barbu <xwave21@gmail.com>
2016-04-26 10:58:46 +0300

    Edit deploy

--- deploy.sh
+++ deploy.sh
@@ -7,13 +7,13 @@ fi
 if [ $TRAVIS_BRANCH == 'master' ]
     then
         echo "Deploying live"
-        envoy run deploy --on=master --branch=master --folder=issuemonitoring
+        envoy run deploy --on=live --branch=master --folder=issuemonitoring
         # curl -X POST --data-urlencode 'payload={"channel": "#deployment-tracking", "username": "Deploy Truck", "text": "Updated http://staging.lendia.ro", "icon_emoji": ":deploytruck:"}' https://hooks.slack.com/services/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
 fi
 
 if [ $TRAVIS_BRANCH == 'staging' ]
     then
         echo "Deploying staging"
-        envoy run deploy --on=staging --branch=staging --folder=dev-issuemonitoring
+        envoy run deploy --on=staging --branch=staging --folder=issuemonitoring
         # curl -X POST --data-urlencode 'payload={"channel": "#deployment-tracking", "username": "Deploy Truck", "text": "Updated https://lendia.ro", "icon_emoji": ":deploytruck:"}' https://hooks.slack.com/services/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
 fi
