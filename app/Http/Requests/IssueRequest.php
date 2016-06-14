6f0e9f54366336f1a0030acdaf380391339114f1
Mihai Barbu <dev.mihai.barbu@gmail.com>
2016-06-14 08:51:47 +0300

    Remove the description limit

--- app/Http/Requests/IssueRequest.php
+++ app/Http/Requests/IssueRequest.php
@@ -6,36 +6,36 @@ use Issue\Http\Requests\Request;
 
 class IssueRequest extends Request
 {
-	/**
-	 * Determine if the user is authorized to make this request.
-	 *
-	 * @return bool
-	 */
-	public function authorize()
-	{
-		return true;
-	}
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
 
-	/**
-	 * Get the validation rules that apply to the request.
-	 *
-	 * @return array
-	 */
-	public function rules()
-	{
-		return [
-			'name' => 'array',
-			'name.ro' => 'required|string|max:1000',
-			'name.en' => 'string|max:1000',
-			'description' => 'array',
-			'description.ro' => 'string|max:1000',
-			'description.en' => 'string|max:1000',
-			'impact' => 'array',
-			'impact.ro' => 'string',
-			'impact.en' => 'string',
-			'status' => 'array',
-			'status.ro' => 'string',
-			'status.en' => 'string'
-		];
-	}
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'array',
+            'name.ro' => 'required|string|max:1000',
+            'name.en' => 'string|max:1000',
+            'description' => 'array',
+            'description.ro' => 'string',
+            'description.en' => 'string',
+            'impact' => 'array',
+            'impact.ro' => 'string',
+            'impact.en' => 'string',
+            'status' => 'array',
+            'status.ro' => 'string',
+            'status.en' => 'string'
+        ];
+    }
 }
