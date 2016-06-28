dc2b9fcddba7108f5bcb7402a6167be17a987f06
Mihai Barbu <dev.mihai.barbu@gmail.com>
2016-06-28 22:54:17 +0300

    Reports display

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -30,8 +30,8 @@ class HomeController extends Controller
         $legalNews = LegalNews::orderBy('created_at', 'desc')->limit(15)->get();
         $legalNews = $legalNews->chunk(5);
 
-        $reports = Report::orderBy('created_at', 'desc')->where('public', '1')->limit(15)->get();
-        $reports = $reports->chunk(5);
+        $reports = Report::orderBy('created_at', 'desc')->where('public', '1')->limit(6)->get();
+        $reports = $reports->chunk(2);
 
         return view('frontend.pages.homepage', compact(['legalNews', 'reports']));
     }
