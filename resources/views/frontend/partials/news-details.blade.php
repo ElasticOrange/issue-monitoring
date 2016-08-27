4e48a5d5811ca4b9988eca6d3c3737401b19d0b3
Mihai Barbu <dev.mihai.barbu@gmail.com>
2016-08-27 14:57:04 +0300

    Issue connected news details show available files for download.

--- resources/views/frontend/partials/news-details.blade.php
+++ resources/views/frontend/partials/news-details.blade.php
@@ -2,27 +2,43 @@
     {{ $news->title }}
 </h3>
 <br>
-<p>
-    <b>Data: </b>{{ $news->date->format('d-m-Y') }}
-</p>
-<p>
-    <b>Extras :</b> {!! strip_tags($news->description) !!}
-</p>
-<p>
-    <b>Stakeholderi: </b>
-    @foreach ($news->connectedStakeholders as $stakeholder)
-    <ul>
-        <li>
-            <a href="{{ action('HomeController@getStakeholderInfo', ['id' => $stakeholder->id, 'name' => Illuminate\Support\Str::slug($stakeholder->name)]) }}">
-                {{ $stakeholder->name }}
-            </a>
-        </li>
-    </ul>
-    @endforeach
-</p>
+@if($news->date)
+    <p>
+        <b>Data: </b>{{ $news->date->format('d-m-Y') }}
+    </p>
+@endif
+@if($news->description)
+    <p>
+        <b>Extras :</b> {!! strip_tags($news->description) !!}
+    </p>
+@endif
+@if($news->fileDocument)
+    <p>
+        <b>Download: </b>
+        <a href="{{ action( "UploadedFileController@downloadFile" , [$news->fileDocument->file_name]) }}" title="{{ $news->fileDocument->original_file_name }}">
+            <i class="fa fa-file-pdf-o"></i>
+        </a>
+    </p>
+@endif
+@if($news->connectedStakeholders)
+    <p>
+        <b>Stakeholderi: </b>
+        @foreach ($news->connectedStakeholders as $stakeholder)
+        <ul>
+            <li>
+                <a href="{{ action('HomeController@getStakeholderInfo', ['id' => $stakeholder->id, 'name' => Illuminate\Support\Str::slug($stakeholder->name)]) }}">
+                    {{ $stakeholder->name }}
+                </a>
+            </li>
+        </ul>
+        @endforeach
+    </p>
+@endif
 <br>
 
-<a href="{{ $news->link }}" target="_blank">
-    Stire completa
-</a>
+@if($news->link)
+    <a href="{{ $news->link }}" target="_blank">
+        Stire completa
+    </a>
+@endif
 <br>
